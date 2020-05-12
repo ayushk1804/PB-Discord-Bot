8032325fef00e159ff685d9f9266cd1f9561ddfb
ayushk780 <40500298+ayushk780@users.noreply.github.com>
2020-05-12 16:28:57 +0530

    Update bot_stats.py
    Add Ram and CPU usage stats.

--- PbBot/plugins/bot_stats.py
+++ PbBot/plugins/bot_stats.py
@@ -9,6 +9,7 @@ import shutil
 from PbBot.utils.utils import humanbytes, get_readable_time
 import asyncio
 from PbBot import botStartTime, Delete_after_duration
+import psutil
 
 # from Bots.Discord.bot import botStartTime # implement later
 
@@ -21,17 +22,23 @@ class BotStats(commands.Cog):
     async def bstats(self, ctx):
         await ctx.message.delete()
         msg = await ctx.send("Checking Bot Stats!")
+        cpufreq = psutil.cpu_freq()
+        svmem = psutil.virtual_memory()
+        swap = psutil.swap_memory()
         currentTime = time.time()-botStartTime
         total, used, free = shutil.disk_usage('.')
         total = humanbytes(total)
         used = humanbytes(used)
         free = humanbytes(free)
-        stats = f'Bot Uptime: {get_readable_time(currentTime)}\n' \
-                f'Total disk space: {total}\n' \
-                f'Used: {used}\n' \
-                f'Free: {free}'
+        stats = f'```Bot Uptime: {time_formatter(currentTime)}```' \
+                f'```Disk Information:\nTotal disk space: {total}' \
+                f'\tUsed: {used}' \
+                f'\tFree: {free}```'
+        stats += f'```CPU Information:\nPhysical cores:{psutil.cpu_count(logical=False)}\tTotal cores: {psutil.cpu_count(logical=True)}\tMax Frequency: {cpufreq.max:.2f}Mhz\nMin Frequency: {cpufreq.min:.2f}Mhz\tCurrent Frequency: {cpufreq.current:.2f}Mhz```'
+        stats += f'```Memory Information\nTotal: {humanbytes(svmem.total)}\tAvailable: {humanbytes(svmem.available)}\tUsed: {humanbytes(svmem.used)}\tPercentage: {svmem.percent}%\n```'
+        stats += f'```Swap Information\nTotal: {humanbytes(swap.total)}\tAvailable: {humanbytes(swap.free)}\tUsed: {humanbytes(swap.used)}\tPercentage: {swap.percent}%```'
         await asyncio.sleep(1.5)
-        await msg.edit(content=stats, delete_after=(Delete_after_duration)*2)
+        await msg.edit(content=stats, delete_after=(Delete_after_duration)*3)
 
 
 def setup(client):
